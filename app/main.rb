e058ad68bd793fa9b970849b3fabaa16699da892
pelgerr <4869909+pelgerr@users.noreply.github.com>
2025-05-03 19:09:06 -0400

    visual updates, add lifespan, add color traits to dna

--- app/main.rb
+++ app/main.rb
@@ -13,12 +13,19 @@ class Boid
     # DNA hash
     if dna.nil?
       @dna = {
+        birth: Kernel.tick_count,
         alignment_weight: Numeric.rand(0.5..2.0),
         cohesion_weight: Numeric.rand(0.5..2.0),
         separation_weight: Numeric.rand(1.5..3.0),
         perception_radius: Numeric.rand(40..100),
         max_speed: Numeric.rand(2.0..5.0),
-        size_w_h: Numeric.rand(8.0..12.0),
+        # size_w_h: Numeric.rand(8.0..12.0),
+        # size_w_h: Numeric.rand(16.0..32.0),
+        size_w: 24,
+        size_h: 18,
+        vis_red: 255,
+        vis_green: 255,
+        vis_blue: 255,
         expired: false
       }
     else
@@ -26,7 +33,7 @@ class Boid
     end
   end
 
-  def update(boids)
+  def update(args, boids)
     # each boid will only be "conscious" of other boids within 50px
     perception_radius = dna.perception_radius
 
@@ -132,17 +139,35 @@ class Boid
     @x %= Grid.w
     @y %= Grid.h
 
-    # expire birds after being alive for 6 seconds
-    # TODO FIX
-    # dna.expired = true if self.created_at_elapsed == 360 && args.state.gen_counter > 1
+    # TODO This is not a good lifespan simulation LOL
+    args.state.boids.shift if args.state.boids.count > 200
+
+    # DEBUG
+    # if args.state.boids.count > 200
+      # lucky = args.state.boids.shift
+      # show_message("#{lucky} has expired")
+    # end
+    # END DEBUG
 
   end
 end
 
 def mutate(src_dna)
   # transform the inheritde dna data
-  mutated = src_dna.transform_values do |val|
-    val.is_a?(Numeric) ? val + Numeric.rand(-1.0..1.0) : val
+  mutated = src_dna.transform_values.with_index do |val, i|
+    case src_dna.keys[i]
+    when :alignment_weight then val + Numeric.rand(-1.0..1.0)
+    when :cohesion_weight then val + Numeric.rand(-1.0..1.0)
+    when :separation_weight then val + Numeric.rand(-1.0..1.0)
+    when :perception_radius then val + Numeric.rand(-1.0..1.0)
+    when :max_speed then val + Numeric.rand(-1.0..1.0)
+    when :size_w then val + Numeric.rand(-3.0..3.0)
+    when :size_h then val + Numeric.rand(-3.0..3.0)
+    when :vis_red then val + Numeric.rand(-50.0..25.0)
+    when :vis_green then val + Numeric.rand(-50.0..25.0)
+    when :vis_blue then val + Numeric.rand(-50.0..25.0)
+    else val
+    end
   end
 
   # clamp values within acceptable ranges
@@ -151,7 +176,11 @@ def mutate(src_dna)
   mutated.separation_weight = mutated.separation_weight.clamp(0.1, 3.0)
   mutated.perception_radius = mutated.perception_radius.clamp(20, 150)
   mutated.max_speed = mutated.max_speed.clamp(1.0, 6.0)
-  mutated.size_w_h = mutated.size_w_h.clamp(6.0, 256.0)
+  mutated.size_w = mutated.size_w.clamp(6.0, 256.0)
+  mutated.size_h = mutated.size_h.clamp(6.0, 256.0)
+  mutated.vis_red = mutated.vis_red.clamp(0.0, 255.0)
+  mutated.vis_green = mutated.vis_green.clamp(0.0, 255.0)
+  mutated.vis_blue = mutated.vis_blue.clamp(0.0, 255.0)
 
   return mutated
 end
@@ -159,23 +188,29 @@ end
 def draw_boids(args)
   args.outputs.sprites << args.state.boids.map do |b|
     {
-      x: b.x - (0.5 * b.dna.size),
-      y: b.y - (0.5 * b.dna.size),
-      w: b.dna.size_w_h,
-      h: b.dna.size_w_h,
+      x: b.x - (0.5 * b.dna.size_w),
+      y: b.y - (0.5 * b.dna.size_h),
+      w: b.dna.size_w,
+      h: b.dna.size_h,
+      r: b.dna.vis_red,
+      g: b.dna.vis_green,
+      b: b.dna.vis_blue,
+      a: 255,
+      blendmode_enum: 1, # 1 --> default blending
       angle: Math.atan2(b.vy, b.vx).to_degrees,
-      path: 'sprites/isometric/red.png'
+      path: 'sprites/isometric/white.png'
     }
   end
 end
 
 def tick(args)
   args.state.gen_counter ||= 1
+  args.outputs.background_color = { r: 30, g: 30, b: 30, a: 255 }
   
   # initialize
-  args.state.boids ||= Array.new(99) { Boid.new(rand(Grid.w), rand(Grid.h)) }
+  args.state.boids ||= Array.new(200) { Boid.new(rand(Grid.w), rand(Grid.h)) }
 
-  # mutate every 5 seconds
+  # create & mutate every 5 seconds
   if  Kernel.tick_count.zmod?(300) && !Kernel.tick_count.zero?
     args.state.gen_counter += 1
     parent = args.state.boids.sample
@@ -183,15 +218,16 @@ def tick(args)
     args.state.boids << Boid.new(parent.x, parent.y, mutate(parent.dna))
 
     # DEBUG
-    p "#{args.state.boids.last} size: #{args.state.boids.last.dna.size_w_h}"
+    # show_message("#{args.state.boids.last} size: #{args.state.boids.last.dna.size_w_h}")
+    # show_message("#{args.state.boids.last} color: #{args.state.boids.last.dna.vis_red}, #{args.state.boids.last.dna.vis_green}, #{args.state.boids.last.dna.vis_blue}")
     # END DEBUG
   end
 
   # update
-  args.state.boids.each { |boid| boid.update(args.state.boids) }
+  args.state.boids.each { |boid| boid.update(args, args.state.boids) }
 
   # destroy dead boids
-  # args.state.boids.reject! { |b| b.dna.expired == true }
+  args.state.boids.reject! { |b| b.dna.expired }
 
   # draw
   draw_boids(args)
@@ -200,7 +236,11 @@ def tick(args)
   # args.outputs.primitives << args.gtk.framerate_diagnostics_primitives
   args.outputs.debug << "#{args.gtk.current_framerate.to_sf}"
   args.outputs.debug << "generations: #{args.state.gen_counter}"
-  args.outputs.debug << "living: #{args.state.boids.length}"
+  args.outputs.debug << "boid count: #{args.state.boids.length}"
   # END DEBUG
 end
 
+def show_message(msg)
+  cur_time = Time.now
+  p "#{cur_time.to_s.split[1]}: #{msg}"
+end
